34a3a37911e673c981716e23eee42432340cd1bc
Create-Project01 <2057245477@qq.com>
2020-04-27 16:14:28 +0800

    新建文件 Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,23 @@
+public void Doctor{
+
+    private Integer id;
+
+    private String name;
+
+    public void setId(Integer id){
+         this.id=id;
+    }
+
+    public Integer getId(){
+	 return this.id;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    
+}
